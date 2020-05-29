8f2432ac6d35aa47af0663cbec6897f06f47573c
LSD <yklsdq@163.com>
2020-05-29 18:02:37 +0800

    #登录

--- SDLoginTools/SDLoginTools/SDLoginTools.swift
+++ SDLoginTools/SDLoginTools/SDLoginTools.swift
@@ -23,5 +23,45 @@ class SDLoginWechat: NSObject {
 //            WXApi.registerApp(wechat.appId, universalLink: wechat.link)
 //        }
 //    }
+    
+    /// openid
+    public var openid: String! {
+        didSet {
+            self.defaults.set(self.openid, forKey: "wechatkit_openid")
+            self.defaults.synchronize()
+        }
+    }
+    /// access token
+    public var accessToken: String! {
+        didSet {
+            self.defaults.set(self.accessToken, forKey: "wechatkit_access_token")
+            self.defaults.synchronize()
+        }
+    }
+    /// refresh token
+    public var refreshToken: String! {
+        didSet {
+            self.defaults.set(self.refreshToken, forKey: "wechatkit_refresh_token")
+            self.defaults.synchronize()
+        }
+    }
+    
+    /// csrf
+    public static var csrfState = "73746172626f796368696e61"
+    /// A shared instance
+    public static let shared: SDLoginWechat = {
+        let instalce = SDLoginWechat()
+        instalce.openid = instalce.defaults.string(forKey: "wechatkit_openid")
+        instalce.accessToken = instalce.defaults.string(forKey: "wechatkit_access_token")
+        instalce.refreshToken = instalce.defaults.string(forKey: "wechatkit_refresh_token")
+        return instalce
+    }()
+
+    /// 检查微信是否已被用户安装
+    ///
+    /// - Returns: 微信已安装返回true，未安装返回false
+    public func isInstalled() -> Bool {
+        return WXApi.isWXAppInstalled()
+    }
 }
 
